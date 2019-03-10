55418cd110ca7b47fbf256a319c6b3e4b68a0e92
DonMins <maks000156@yandex.ru>
2019-03-10 14:10:30 +0400

    коменты чуть чуть

--- src/SaveStateCircle.java
+++ src/SaveStateCircle.java
@@ -7,9 +7,15 @@ import java.io.FileOutputStream;
 public class SaveStateCircle {
 
 public static void saveInXml(Circle circle) throws JAXBException, FileNotFoundException {
+
+    //создание объекта Marshaller, который выполняет сериализацию
     JAXBContext context = JAXBContext.newInstance(Circle.class);
     Marshaller marshaller = context.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+    // вывод xml в формотированном виде
+      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+    // сама сериализация
     marshaller.marshal(circle, new FileOutputStream("state.xml"));
 }
 }
--- src/XmlParser.java
+++ src/XmlParser.java
@@ -14,14 +14,20 @@ public class XmlParser {
     public static Circle getXmlData() throws ParserConfigurationException, IOException, SAXException {
 
             File inputFile = new File("state.xml");
+
+            //получаем новый экземпляр DocumentBuilderFactory
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            // create a new DocumentBuilder
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
+
+           //Это вспомогательный атрибут, который обеспечивает прямой доступ к дочернему узлу, который является элементом документа
+           //normalize действует так, как если бы документ проходил цикл сохранения и загрузки, переводя документ в «нормальную» форму.
             doc.getDocumentElement().normalize();
+
             Circle circle = new Circle();
             NodeList nList = doc.getElementsByTagName("circle");
-
-                Node nNode = nList.item(0);
+            Node nNode = nList.item(0);
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
